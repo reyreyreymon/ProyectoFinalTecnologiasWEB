e6f879da5c1af5664b1ba3dcde9393a18bc23d34
marie <marie.serna11@gmail.com>
2020-06-27 17:27:44 -0500

    test

--- Pagina/src/app/paginas_inicio/admin/empleados/empleados.component.ts
+++ Pagina/src/app/paginas_inicio/admin/empleados/empleados.component.ts
@@ -1,23 +1,26 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
 import { CrudService } from "../../../service/crud/crud.service";
-import {FormControl, FormBuilder, Validators} from "@angular/forms";
-import {MatSnackBar} from '@angular/material/snack-bar';
-import { LoginComponent } from '../../login/login.component';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
-import { ConfirmacionComponent } from 'src/app/dialogos/confirmacion/confirmacion.component';
+import { FormControl, FormBuilder, Validators } from "@angular/forms";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from "@angular/material/dialog";
+import { ConfirmacionComponent } from "src/app/dialogos/confirmacion/confirmacion.component";
 
 export interface DialogData {
   animal: string;
   name: string;
+  estado: string;
 }
 
 @Component({
-  selector: 'app-empleados',
-  templateUrl: './empleados.component.html',
-  styleUrls: ['./empleados.component.css']
+  selector: "app-empleados",
+  templateUrl: "./empleados.component.html",
+  styleUrls: ["./empleados.component.css"],
 })
 export class EmpleadosComponent implements OnInit {
-
+  //variables del empleado
   empleados: any;
   empleado_nombe: string;
   empleado_apellido: string;
@@ -25,85 +28,101 @@ export class EmpleadosComponent implements OnInit {
   empleado_puesto: string;
   empleado_salario: number;
 
-  editable:boolean = false;
-  mensaje: string;//alta
+  editable: boolean = false;
+  mensaje: string; //alta
 
   form;
 
+  //variables del dialogo
   animal: string;
   name: string;
+  estado_Creacion: string = "";
+
+  puesto:string;
 
   constructor(
-    public crudService:CrudService,
-    private formBuilder:FormBuilder,
-    private _snackBar: MatSnackBar,
-    public dialog: MatDialog) {
-
+    public crudService: CrudService,
+    private formBuilder: FormBuilder,
+    public dialog: MatDialog
+  ) {
     this.form = formBuilder.group({
-      empleado_nombe1: ['', Validators.required],
-      empleado_apellido1: ['', Validators.required],
-      empleado_ano1: ['', [Validators.required]],
-      empleado_salario1: ['', Validators.required],
-      empleado_puesto1: ['', Validators.required]
+      empleado_nombe1: ["", Validators.required],
+      empleado_apellido1: ["", Validators.required],
+      empleado_ano1: ["", [Validators.required]],
+      empleado_salario1: ["", Validators.required],
+      empleado_puesto1: ["", Validators.required],
     });
+  }
 
-   }
+  submit() {
+    //this.estado_Creacion = 'empleado_no_creado';
+    //this.openDialog();
+    this.crearEmpleado();
+    //this.estado_Creacion = '';
+  }
 
-   submit(){
-    this.Dialogo();
-     this.crearEmpleado();
-   }
-
-  crearEmpleado(){
-    let Record ={};
-    Record['nombre'] = this.empleado_nombe;
-    Record['apellido'] = this.empleado_apellido;
-    Record['ano'] = this.empleado_ano;
-    Record['puesto'] = this.empleado_puesto;
-    Record['salario'] = this.empleado_salario;
-    if(this.empleado_nombe==="" || this.empleado_apellido ==="" || this.empleado_ano === null|| this.empleado_nombe==="" || this.empleado_puesto==="" || this.empleado_salario == null){
+  crearEmpleado() {
+    let Record = {};
+    Record["nombre"] = this.empleado_nombe;
+    Record["apellido"] = this.empleado_apellido;
+    Record["ano"] = this.empleado_ano;
+    Record["puesto"] = this.empleado_puesto;
+    Record["salario"] = this.empleado_salario;
+    if (
+      this.empleado_nombe === "" ||
+      this.empleado_apellido === "" ||
+      this.empleado_ano === null ||
+      this.empleado_nombe === "" ||
+      this.empleado_puesto === "" ||
+      this.empleado_salario == null
+    ) {
+      //Dialogo
+      this.estado_Creacion = "empleado_no_creado";
+      this.openDialog();
       return;
     }
+    this.crudService
+      .crear_Nuevo_Empleado(Record)
+      .then((res) => {
+        this.empleado_nombe = "";
+        this.empleado_apellido = "";
+        this.empleado_ano = null;
+        this.empleado_puesto = "";
+        this.empleado_salario = null;
+        console.log(res);
+        //Dialogo
+        this.variables_Dialogo(this.empleado_nombe, this.empleado_apellido);
+        this.estado_Creacion = "empleado_creado";
+        this.openDialog();
+      })
+      .catch((error) => {
+        //Dialogo
+        this.estado_Creacion = "empleado_no_creado";
+        this.openDialog();
 
-    this.crudService.crear_Nuevo_Empleado(Record)
-    .then(res =>{
-      this.empleado_nombe = "";
-      this.empleado_apellido = "";
-      this.empleado_ano = null;
-      this.empleado_puesto = "";
-      this.empleado_salario = null;
-
-      console.log(res);
-      this.mensaje = "Empleado creado correctamente"
-      this._snackBar.open("Producto agredado", "Correctamente", {
-        duration: 2000,
-    });
-
-    }).catch(error =>{
-      console.log(error);
-    });
+        console.log(error);
+      });
   }
 
   ngOnInit(): void {
     //Para mostrar los empleados, obtenemos el arreglo
-    this.crudService.obtener_Empleados()
-    .subscribe(data => {
-      this.empleados = data.map(e => {
+    this.crudService.obtener_Empleados().subscribe((data) => {
+      this.empleados = data.map((e) => {
         return {
           id: e.payload.doc.id,
-          editable:false,
-          nombre: e.payload.doc.data()['nombre'],
-          apellido: e.payload.doc.data()['apellido'],
-          ano: e.payload.doc.data()['ano'],
-          puesto: e.payload.doc.data()['puesto'],
-          salario: e.payload.doc.data()['salario']
+          editable: false,
+          nombre: e.payload.doc.data()["nombre"],
+          apellido: e.payload.doc.data()["apellido"],
+          ano: e.payload.doc.data()["ano"],
+          puesto: e.payload.doc.data()["puesto"],
+          salario: e.payload.doc.data()["salario"],
         };
-      })
+      });
       console.log(this.empleados);
     });
   }
 
-  editarEmpledao(Record){
+  editarEmpledao(Record) {
     Record.editable = true;
     Record.editnombre = Record.nombre;
     Record.editapellido = Record.apellido;
@@ -112,37 +131,50 @@ export class EmpleadosComponent implements OnInit {
     Record.editsalario = Record.salario;
   }
 
-  actualizarEmpleado(item){
-    let Record ={};
-    Record['nombre'] = item.editnombre;
-    Record['apellido'] = item.editapellido;
-    Record['ano'] = item.editano;
-    Record['puesto'] = item.editpuesto;
-    Record['salario'] = item.editsalario;
+  actualizarEmpleado(item) {
+    let Record = {};
+    Record["nombre"] = item.editnombre;
+    Record["apellido"] = item.editapellido;
+    Record["ano"] = item.editano;
+    Record["puesto"] = item.editpuesto;
+    Record["salario"] = item.editsalario;
+    //variables para el dialogo
+    this.variables_Dialogo(item.editnombre, item.editapellido);
     //mandamos al servicio.
-    this.crudService.actualizar_Empledao(item.id,Record);
+    this.crudService.actualizar_Empledao(item.id, Record);
     //bloqueamos los input para que ya no sean editables.
     item.editable = false;
+    //dialogo
+    this.estado_Creacion = "empleado_actualizado";
+    this.openDialog();
   }
 
-  borrarEmpleado(id){
+  borrarEmpleado(id) {
     this.crudService.eliminar_Empleado(id);
+    //Dialogo
+    this.estado_Creacion = "empleado_eliminado";
+    this.openDialog();
   }
 
-  Dialogo(){
-    this.dialog.open(LoginComponent);
-  }
-
+  //dialogos de informacion
   openDialog(): void {
     const dialogRef = this.dialog.open(ConfirmacionComponent, {
-      width: '250px',
-      data: {name: this.name, animal: this.animal}
+      height: "265px",
+      width: "400px",
+      data: {
+        name: this.name,
+        animal: this.animal,
+        estado: this.estado_Creacion,
+      },
     });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log("The dialog was closed");
       this.animal = result;
     });
   }
 
+  variables_Dialogo(nombre: string, apellido: string) {
+    this.name = nombre;
+    this.animal = apellido;
+  }
 }
--- Pagina/src/app/paginas_inicio/admin/productos/productos.component.ts
+++ Pagina/src/app/paginas_inicio/admin/productos/productos.component.ts
@@ -1,92 +1,115 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { CrudService } from "../../../service/crud/crud.service";
-import {MatSnackBar} from '@angular/material/snack-bar';
-import {FormControl, FormBuilder, Validators} from "@angular/forms";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { FormControl, FormBuilder, Validators } from "@angular/forms";
+import {  MatDialog,  MatDialogRef,  MAT_DIALOG_DATA,} from "@angular/material/dialog";
+import { ConfirmacionComponent } from "src/app/dialogos/confirmacion/confirmacion.component";
+
+export interface DialogData {
+  animal: string;
+  name: string;
+  estado: string;
+}
 
 @Component({
-  selector: 'app-productos',
-  templateUrl: './productos.component.html',
-  styleUrls: ['./productos.component.css']
+  selector: "app-productos",
+  templateUrl: "./productos.component.html",
+  styleUrls: ["./productos.component.css"],
 })
 export class ProductosComponent implements OnInit {
-
   productos: any;
   producto_descripcion: string;
   producto_marca: string;
   producto_precio: number;
   producto_existencia: number;
 
-  editable:boolean = false;
-  mensaje: string;//alta
+  editable: boolean = false;
+  mensaje: string; //alta
 
   form;
 
-  constructor(public crudService:CrudService, private _snackBar: MatSnackBar, private formBuilder:FormBuilder) {
+
+  //variables del dialogo
+  animal: string;
+  name: string;
+  estado_Creacion: string = "";
+
+  constructor(
+    public crudService: CrudService,
+    private formBuilder: FormBuilder,
+    public dialog: MatDialog
+  ) {
     this.form = formBuilder.group({
-      producto_descripcion1: ['', Validators.required],
-      producto_marca1: ['', Validators.required],
-      producto_precio1: ['', [Validators.required]],
-      producto_existencia1: ['', [Validators.required]]
+      producto_descripcion1: ["", Validators.required],
+      producto_marca1: ["", Validators.required],
+      producto_precio1: ["", [Validators.required]],
+      producto_existencia1: ["", [Validators.required]],
     });
   }
 
-  submit(){
-    console.log(this.form.value);
+  submit() {
     this.crearProducto();
   }
 
-  crearProducto(){
-    let Record ={};
-    Record['descripcion'] = this.producto_descripcion;
-    Record['marca'] = this.producto_marca;
-    Record['precio'] = this.producto_precio;
-    Record['existencia'] = this.producto_existencia;
+  crearProducto() {
+    let Record = {};
+    Record["descripcion"] = this.producto_descripcion;
+    Record["marca"] = this.producto_marca;
+    Record["precio"] = this.producto_precio;
+    Record["existencia"] = this.producto_existencia;
 
-    if(this.producto_descripcion==="" || this.producto_marca ==="" || this.producto_precio === null || this.producto_existencia===null){
+    if (
+      this.producto_descripcion === "" ||
+      this.producto_marca === "" ||
+      this.producto_precio === null ||
+      this.producto_existencia === null
+    ) {
+      //Dialogo
+      this.estado_Creacion = "producto_no_creado";
+      this.openDialog();
       return;
     }
 
-    this.crudService.crear_Nuevo_Producto(Record)
-    .then(res =>{
-      this.producto_descripcion = "";
-      this.producto_marca = "";
-      this.producto_precio = null;
-      this.producto_existencia = null;
+    this.crudService
+      .crear_Nuevo_Producto(Record)
+      .then((res) => {
+        this.producto_descripcion = "";
+        this.producto_marca = "";
+        this.producto_precio = null;
+        this.producto_existencia = null;
 
-      console.log(res);
-      this._snackBar.open("Producto agredado", "Correctamente", {
-          duration: 2000,
+        console.log(res);
+        //Dialogo
+        this.variables_Dialogo(this.producto_descripcion, this.producto_marca);
+        this.estado_Creacion = "producto_creado";
+        this.openDialog();
+      })
+      .catch((error) => {
+        //Dialogo
+        this.estado_Creacion = "producto_no_creado";
+        this.openDialog();
+        console.log(error);
       });
-
-    }).catch(error =>{
-
-      this._snackBar.open("Error en agregar produto", error, {
-        duration: 2000,
-    });
-      console.log(error);
-
-    });
   }
 
   ngOnInit(): void {
     //Para mostrar los prodcutos, obtenemos el arreglo
-    this.crudService.obtener_Productos()
-    .subscribe(data => {
-      this.productos = data.map(e => {
+    this.crudService.obtener_Productos().subscribe((data) => {
+      this.productos = data.map((e) => {
         return {
           id: e.payload.doc.id,
-          editable:false,
-          descripcion: e.payload.doc.data()['descripcion'],
-          marca: e.payload.doc.data()['marca'],
-          precio: e.payload.doc.data()['precio'],
-          existencia: e.payload.doc.data()['existencia']
+          editable: false,
+          descripcion: e.payload.doc.data()["descripcion"],
+          marca: e.payload.doc.data()["marca"],
+          precio: e.payload.doc.data()["precio"],
+          existencia: e.payload.doc.data()["existencia"],
         };
-      })
+      });
       console.log(this.productos);
     });
   }
 
-  editarProducto(Record){
+  editarProducto(Record) {
     Record.editable = true;
     Record.editdescripcion = Record.descripcion;
     Record.editmarca = Record.marca;
@@ -94,27 +117,47 @@ export class ProductosComponent implements OnInit {
     Record.editexistencia = Record.existencia;
   }
 
-  actualizarProducto(item){
-    let Record ={};
-    Record['descripcion'] = item.editdescripcion;
-    Record['marca'] = item.editmarca;
-    Record['precio'] = item.editprecio;
-    Record['existencia'] = item.editexistencia;
+  actualizarProducto(item) {
+    let Record = {};
+    Record["descripcion"] = item.editdescripcion;
+    Record["marca"] = item.editmarca;
+    Record["precio"] = item.editprecio;
+    Record["existencia"] = item.editexistencia;
+    //variables para el dialogo
+    this.variables_Dialogo(item.editdescripcion, item.editmarca);
     //mandamos al servicio.
-    this.crudService.actualizar_Producto(item.id,Record);
+    this.crudService.actualizar_Producto(item.id, Record);
     //bloqueamos los input para que ya no sean editables.
     item.editable = false;
-    this._snackBar.open("Producto actualizado", "Correctamente", {
-      duration: 2000,
-  });
+    //dialogo
+    this.estado_Creacion = "producto_actualizado";
+    this.openDialog();
   }
 
-  borrarProducto(id){
+  borrarProducto(id) {
     this.crudService.eliminar_Producto(id);
-    this._snackBar.open("Producto eliminado de la base", "Correctamente", {
-      duration: 2000,
-  });
+    this.estado_Creacion = "producto_eliminado";
+    this.openDialog();
   }
 
-}
+  //dialogos de informacion
+  openDialog(): void {
+    const dialogRef = this.dialog.open(ConfirmacionComponent, {
+      width: "300px",
+      data: {
+        name: this.name,
+        animal: this.animal,
+        estado: this.estado_Creacion,
+      },
+    });
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log("The dialog was closed");
+      this.animal = result;
+    });
+  }
 
+  variables_Dialogo(nombre: string, apellido: string) {
+    this.name = nombre;
+    this.animal = apellido;
+  }
+}
