8cf8d481fb071783d61334795bf92eb035c3eaa3
jessie <jessie.gc2213@gmail.com>
2020-06-28 14:35:53 -0500

    GCDG_Log_Lec

--- Pagina/src/app/paginas_inicio/admin/empleados/empleados.component.ts
+++ Pagina/src/app/paginas_inicio/admin/empleados/empleados.component.ts
@@ -3,6 +3,7 @@ import { CrudService } from "../../../service/crud/crud.service";
 import { FormBuilder, Validators } from "@angular/forms";
 import {  MatDialog } from "@angular/material/dialog";
 import { ConfirmacionComponent } from "src/app/dialogos/confirmacion/confirmacion.component";
+import Speech from 'speak-tts';//importamos el lector
 
 export interface DialogData {
   animal: string;
@@ -16,6 +17,10 @@ export interface DialogData {
   styleUrls: ["./empleados.component.css"],
 })
 export class EmpleadosComponent implements OnInit {
+  //variables para el lector de pantalla
+  result_Lector = '';
+  speech: any;
+
   //variables del empleado
   empleados: any;
   empleado_nombe: string;
@@ -48,6 +53,61 @@ export class EmpleadosComponent implements OnInit {
       empleado_salario1: ["", Validators.required],
       empleado_puesto1: ["", Validators.required],
     });
+
+    //Iniciamos lec de pantalla
+    this.speech = new Speech() // will throw an exception if not browser supported
+    if(this.speech .hasBrowserSupport()) { // returns a boolean
+        console.log("speech synthesis supported")
+        this.speech.init({
+                'volume': 1,
+                'lang': 'es-MX',
+                'rate': 1,
+                'pitch': 1,
+                'voice':'Google UK English Male',
+                'splitSentences': true,
+                'listeners': {
+                    'onvoiceschanged': (voices) => {
+                        console.log("Event voiceschanged", voices)
+                    }
+                }
+        }).then((data) => {
+            // The "data" object contains the list of available voices and the voice synthesis params
+            console.log("Lector está listo", data)
+            data.voices.forEach(voice => {
+            console.log(voice.name + " "+ voice.lang)
+            });
+        }).catch(e => {
+            console.error("Ocurrió un error al inicializar: ", e)
+        })
+    }
+
+  }//cierra constructor
+
+  //comenzar lector
+  start(){
+    var temporalDivElement = document.createElement("div");
+    //leemos lo que esta en el id readNow
+    temporalDivElement.innerHTML = document.getElementById("readNow").innerHTML;
+    //validamos cualquier tipo de texto (con estilos o no)
+    this.result_Lector = temporalDivElement.textContent || temporalDivElement.innerText || "";
+
+      this.speech.speak({
+          text: this.result_Lector,
+      }).then(() => {
+          console.log("Exito")
+      }).catch(e => {
+          console.error("Ocurrió un error:", e) 
+      })
+  }
+
+  //detener lector
+  pause(){
+    this.speech.pause();
+  }
+  
+  //Renaudamos el lector
+  resume(){
+    this.speech.resume();
   }
 
   submit() {
--- Pagina/src/app/paginas_inicio/inicio/inicio.component.ts
+++ Pagina/src/app/paginas_inicio/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Speech from 'speak-tts';//importamos el lector
 
 @Component({
   selector: 'app-inicio',
@@ -8,8 +9,65 @@ import { Component, OnInit } from '@angular/core';
 export class InicioComponent implements OnInit {
   members:any[] = [];
 
-  constructor() {
+  //variables para el lector de pantalla
+  result_Lector = '';
+  speech: any;
 
+
+  constructor() {
+    //Iniciamos lec de pantalla
+    this.speech = new Speech() // will throw an exception if not browser supported
+    if(this.speech .hasBrowserSupport()) { // returns a boolean
+        console.log("speech synthesis supported")
+        this.speech.init({
+                'volume': 1,
+                'lang': 'es-MX',
+                'rate': 1,
+                'pitch': 1,
+                'voice':'Google UK English Male',
+                'splitSentences': true,
+                'listeners': {
+                    'onvoiceschanged': (voices) => {
+                        console.log("Event voiceschanged", voices)
+                    }
+                }
+        }).then((data) => {
+            // The "data" object contains the list of available voices and the voice synthesis params
+            console.log("Lector está listo", data)
+            data.voices.forEach(voice => {
+            console.log(voice.name + " "+ voice.lang)
+            });
+        }).catch(e => {
+            console.error("Ocurrió un error al inicializar: ", e)
+        })
+    }
+  }//cierra constructor
+
+   //comenzar lector
+   start(){
+    var temporalDivElement = document.createElement("div");
+    //leemos lo que esta en el id readNow
+    temporalDivElement.innerHTML = document.getElementById("readNow").innerHTML;
+    //validamos cualquier tipo de texto (con estilos o no)
+    this.result_Lector = temporalDivElement.textContent || temporalDivElement.innerText || "";
+
+      this.speech.speak({
+          text: this.result_Lector,
+      }).then(() => {
+          console.log("Exito")
+      }).catch(e => {
+          console.error("Ocurrió un error:", e) 
+      })
+  }
+
+  //detener lector
+  pause(){
+    this.speech.pause();
+  }
+  
+  //Renaudamos el lector
+  resume(){
+    this.speech.resume();
   }
 
   ngOnInit(): void {
